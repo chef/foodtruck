a88921f4b5e8ca3a1243249d48ea8ecf8a4d7d4c
Nathan Cerny <ncerny@chef.io>
2020-09-21 13:47:55 -0500

    WIP -- Break eventhubs into functions
    Signed-off-by: Nathan Cerny <ncerny@chef.io>

--- cmd/listen.go
+++ cmd/listen.go
@@ -13,16 +13,10 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
 package cmd
 
 import (
-	"context"
-	"fmt"
-	"os"
-	"os/signal"
-	"time"
-
-	eventhub "github.com/Azure/azure-event-hubs-go/v3"
 	"github.com/spf13/cobra"
 )
 
@@ -37,70 +31,7 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		offset := "0"
 
-		hostname, err := os.Hostname()
-		if err != nil {
-			panic(err)
-		}
-
-		fmt.Println("hostname:", hostname)
-
-		connStr := "" + hostname
-		hub, err := eventhub.NewHubFromConnectionString(connStr)
-
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
-		defer cancel()
-
-		// send a single message into a random partition
-		err = hub.Send(ctx, eventhub.NewEventFromString("hello, world!"))
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		handler := func(c context.Context, event *eventhub.Event) error {
-			fmt.Println("Handler!")
-			fmt.Println(string(event.Data))
-			return nil
-		}
-
-		// listen to each partition of the Event Hub
-		runtimeInfo, err := hub.GetRuntimeInformation(ctx)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		for _, partitionID := range runtimeInfo.PartitionIDs {
-			// Start receiving messages
-			//
-			// Receive blocks while attempting to connect to hub, then runs until listenerHandle.Close() is called
-			// <- listenerHandle.Done() signals listener has stopped
-			// listenerHandle.Err() provides the last error the receiver encountered
-			// listenerHandle
-			_, err := hub.Receive(ctx, partitionID, handler, eventhub.ReceiveWithStartingOffset(offset))
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			fmt.Println("Receive!")
-		}
-
-		// Wait for a signal to quit:
-		signalChan := make(chan os.Signal, 1)
-		signal.Notify(signalChan, os.Interrupt, os.Kill)
-		<-signalChan
-
-		err = hub.Close(context.Background())
-		if err != nil {
-			fmt.Println(err)
-		}
 	},
 }
 
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/chef/foodtruck
 go 1.15
 
 require (
+	github.com/Azure/azure-amqp-common-go/v3 v3.0.0
 	github.com/Azure/azure-event-hubs-go/v3 v3.3.0
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/spf13/cobra v1.0.0
